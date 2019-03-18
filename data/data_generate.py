039c39c9ca5acad4f0308f3427b0b1ddf7677fbb
Christoph Becker <christoph.becker@durham.ac.uk>
2019-03-18 20:12:11 +0000

    read_hdf5 correction

--- data/data_generate.py
+++ data/data_generate.py
@@ -15,33 +15,25 @@ args["nsnap"]    = sys.argv[3]
 args["nvoxel"]   = sys.argv[4]
 
 s = read_hdf5.snapshot(args["nsnap"], args["simdir"]) 
-s.read(["Coordinates"], parttype=[0])
-print(type(sdata['Coordinates']['dm']))
+s.read(["Coordinates"], parttype=[1])
+dm_pos = (s['Coordinates']['dm']).astype('float64')
+print(np.max(dm_pos[:,0]))
 
-
-dm_pos = (sdata['Coordinates']['dm']*scale).astype('float64')
-
-stable=pd.DataFrame(dm_pos)
-postable.columns=(['x','y','z'])
-postable['x_b']=np.floor(postable['x']/(75000/1024)) 
-postable['y_b']=np.floor(postable['y']/(75000/1024))
-postable['z_b']=np.floor(postable['z']/(75000/1024))
-postable['x_b'] = postable['x_b'].astype(int)
-postable['y_b'] = postable['y_b'].astype(int)
-postable['z_b'] = postable['z_b'].astype(int)
+dmdf=pd.DataFrame({
+    'x_vox' : (np.floor((dm_pos[:, 0]/62000/args["nvoxel"]))).astype(int),
+    'y_vox' : (np.floor((dm_pos[:, 1]/62000/args["nvoxel"]))).astype(int),
+    'z_vox' : (np.floor((dm_pos[:, 2]/62000/args["nvoxel"]))).astype(int),
+})
 
 mergetable['c']=1                                                            
 def dataframe_to_array(df, out_shp):                                         
-    ids = np.ravel_multi_index(df[['x_b','y_b','z_b']].values.T, out_shp)    
+    ids = np.ravel_multi_index(df[['x_vox','y_voxb','z_vox']].values.T, out_shp)    
     val = df['count'].values                                                 
     return np.bincount(ids, val, minlength=np.prod(out_shp)).reshape(out_shp)
 
-counts=mergetable.groupby(['x_b','y_b','z_b'])['c'].count().reset_index(name="count")
+counts=mergetable.groupby(['x_vox','y_vox','z_vox'])['c'].count().reset_index(name="count")
 arr=dataframe_to_array(counts, (1024,1024,1024))                             
 old_arr=np.load('/scratch/xz2139/Dark_zeros.npy')                            
 np.save('/scratch/xz2139/Dark_zeros.npy',old_arr+arr)                        
 print(name) 
 
-x_out = Pa['Pos'][binds, 0]
-y_out = Pa['Pos'][binds, 1]
-z_out = Pa['Pos'][binds, 2]
