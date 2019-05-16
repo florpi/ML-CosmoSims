d27c08fb4d0748cbd2f8e1285a3b286cd56aa1df
florpi <florpisoteadadejardin@gmail.com>
2019-05-16 06:39:17 +0100

    main updated

--- data/Dataset.py
+++ data/Dataset.py
@@ -1,116 +1,34 @@
 from torch.utils import data
 import numpy as np
+import h5py
 
+#based on https://stanford.edu/~shervine/blog/pytorch-how-to-generate-data-parallel
 
 class Dataset(data.Dataset):
-    """
-    Class to control dataset input into ML-algorithm.
-    """
 
-    def __init__(
-        self,
-        catalogue_ids,
-        features_file,
-        targets_file,
-        cat="count",
-        vel=False,
-        aug=False,
-        reg=False,
-        normalize=False,
-    ):
-        """
-        Initialization
+	def __init__(self, list_IDs):
 
-        Parameters
-        ----------
-        data_path : str
-            The path to the file in which data-set is found
-        label_path : str
-            The path to the file in which label-set is found
-        cat : str
-            Define which target data to use. Can be either 'mass' or 'count'
-        vel : boolean
-            Switch to use object velocity or not.
-        aug : boolean
-            Switch for data augmentation or not.
-        reg : boolean
-            ???
-        """
-        self.ids = catalogue_ids
-        self.features_file = features_file
-        self.targets_file = targets_file
-        self.target = cat
-        self.aug = aug
-        self.reg = reg
-        self.vel = vel
-        self.normalize = normalize
+		self.list_IDs = list_IDs
 
-    def __getitem__(
-            self,
-            index,
-    ):
-        """
-        Parameters
-        ----------
-        index : list
-            Coordinates of the voxels in the data-set
-        **params : dict
-            batch-size, shuffle, num_workers
-        
-        Returns
-        -------
-        feature_boxes : np.ndarray
-            Dark-matter input
-        target_boxes : np.ndarray
-            Target values e.g. single stars or galaxies
-        """
-        # Load dark-matter catalogue
-        feature_boxes = np.load(self.features_file)  #3D
-        feature_boxes = [feature_boxes[xx] for xx in self.ids]
-        feature_boxes = np.expand_dims(feature_boxes, axis=0)  #4D
+	def __len__(self):
+		'Denotes the total number of samples'
+		return len(self.list_IDs)
 
-        if self.normalize is True:
-            # Normalize dark-matter data w.r.t. maximum
-            feature_boxes = feature_boxes / np.max(feature_boxes)
 
-        if self.target == "count":
-            # Use nr. of stars as target
-            target_boxes = np.load(self.targets_file)
-            target_boxes = [target_boxes[xx] for xx in self.ids]
-            if not self.reg:
-                # Convert python function to vector function
-                convert = np.vectorize(self.convert_class)
-                target_boxes = convert(target_boxes)
+	def __getitem__(self, index):
+		'Generates one sample of data'
+		# Select sample
+		ID = self.list_IDs[index]
 
-        elif self.target == "mass":
-            # use mass as target
-            target_boxes = np.load(self.targets_file)
+		data_filename = "/home/zcxn55/ML-CosmoSims/data/test.hdf5"
+		labels_filename = "/home/zcxn55/ML-CosmoSims/data/labels.hdf5"
+		# Load data and get label
+		#X = torch.load('data/' + ID + '.pt')
+		X = h5py.File(data_filename)[str(ID)][...]
 
-        if self.aug is True:
-            # Dataset augmentation
-            dim_to_flip = tuple(
-                np.arange(3)[np.random.choice(a=[False, True], 
-                size=3)]
-            )
-            if len(dim_to_flip) > 0:
-                dmboxes = np.flip(feature_boxes, dim_to_flip)
-                target_boxes = np.flip(target_boxes, dim_to_flip)
+		y = h5py.File(labels_filename)[str(ID)][...]
 
-        #if self.vel is True:
-        #    # Add velocity info. to dm-dataset
-        #    veloset = np.load()
-        #    dmboxes = np.concatenate((dmboxes, veloset), axis=0)
 
-        return feature_boxes, target_boxes
+		return X, y
 
-    def __len__(self):
-        "Denotes the total number of samples"
-        return len(self.ids)
 
-    def convert_class(self, num):
-        if num==0:
-            return 0
-        elif num>0:
-            return 1
-        else:
-            print('dark matter mass smaller than 0')
--- main.py
+++ main.py
@@ -1,294 +1,84 @@
-import argparse
-import os, sys, glob, time
-import random
-import shutil
-import warnings
-
+import os
 import torch
-import torch.nn as nn
-import torch.backends.cudnn as cudnn
-import torch.optim
-import torch.utils.data
-import torch.nn.functional as F
-
-from torch.utils import data
-import random
 import numpy as np
-from itertools import product
-
-from parse_args import parse_args
-import train
-import validate
-import initial_loss
-
-sys.path.insert(0, "/cosma7/data/dp004/dc-beck3/Dark2Light/data/")
-from Dataset import Dataset
-
-sys.path.insert(0, "/cosma7/data/dp004/dc-beck3/Dark2Light/networks/")
-from Models import *
-
-# sys.path.insert(0, '/cosma7/data/dp004/dc-beck3/Dark2Light/training/')
-# from train_f import *
-    
-# Find out if GPUs or CPUs are available
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print("This computer will use: %s" % device)
-
-# the following variables are global variables that record the statistics
-# for each epoch so that the plot can be produced
-TRAIN_LOSS, VAL_LOSS, VAL_ACC, VAL_RECALL, VAL_PRECISION = [], [], [], [], []
-BEST_VAL_LOSS = 999999999
-BEST_RECALL = 0
-BEST_PRECISION = 0
-BEST_F1SCORE = 0
-BEST_ACC = 0
-EPSILON = 1e-5
-
-def initial_loss(train_loader, val_loader, model, criterion, target_class):
-    # AverageMeter is a object that record the sum,
-    # avg, count and val of the target stats
-    train_losses = AverageMeter()
-    val_losses = AverageMeter()
-    correct = 0
-    # ptotal = 0  #count of all positive predictions
-    # tp = 0    #true positive
-    total = 0  # total count of data
-    TPRs = AverageMeter()
-    FPRs = AverageMeter()
-    # switch to train mode
-    model.eval()
-
-    with torch.no_grad():
-        for i, (input, target) in enumerate(train_loader):
-            # add a dimension, from (1, 32, 32, 32) to (1,1,32,32,32)
-            input = input.to(device).float()
-            if target_class == 0:
-                target = target.to(device).long()
-            elif target_class == 1:
-                target = target.to(device).float()
-            # compute output
-            output = model(input)
-            # print("target1: ", target.size())
-            # print("output: ", output.size())
-            loss = criterion(output, target)
-            # measure accuracy and record loss
-            train_losses.update(loss.item(), input.size(0))
-
-        for i, (input, target) in enumerate(val_loader):
-            # add a dimension, from (1, 32, 32, 32) to (1,1,32,32,32)
-            input = input.to(device).float()
-            if target_class == 0:
-                target = target.to(device).long()
-            elif target_class == 1:
-                target = target.to(device).float()
-            # compute output
-            output = model(input)
-            loss = criterion(output, target)
-            # measure accuracy and record loss
-            val_losses.update(loss.item(), input.size(0))
-            if target_class == 0:
-                outputs = F.softmax(output, dim=1)
-                predicted = outputs.max(1)[1]
-                total += np.prod(target.shape)
-                correct += predicted.eq(target.view_as(predicted)).sum().item()
-                TPR, gp, FPR, gf = confusion_matrix_calc(predicted, target)
-                TPRs.update(TPR, gp)
-                FPRs.update(FPR, gf)
-            loss = criterion(output, target)
-            val_losses.update(loss.item(), input.size(0))
-
-    if target_class == 0:
-        acc = correct / total * 100
-        recall = TPRs.avg * 100
-        precision = TPRs.sum / (TPRs.sum + FPRs.sum + EPSILON) * 100
-        VAL_RECALL.append(recall)
-        VAL_ACC.append(acc)
-        VAL_PRECISION.append(precision)
-
-    TRAIN_LOSS.append(train_losses.avg)
-    VAL_LOSS.append(val_losses.avg)
-    if target_class == 0:
-        print(
-            "Epoch Train Loss {train_losses.avg:.4f},\
-             Test Loss {val_losses.avg:.4f},\
-             Test Accuracy {acc:.4f},\
-             Test Recall {recall:.4f}\t Precision {precision:.4f}\t".format(
-                train_losses=train_losses,
-                val_losses=val_losses,
-                acc=acc,
-                recall=recall,
-                precision=precision,
-            )
-        )
-    else:
-        print(
-            "Epoch Train Loss {train_losses.avg:.4f},\
-             Test Loss {val_losses.avg:.4f}".format(
-                train_losses=train_losses, val_losses=val_losses
-            )
-        )
+from torch.utils import data
+import torch.nn as nn
+from data.Dataset import Dataset
+from networks.SegNet import SegNet
+from torchsummary import summary
 
 
-def main():
-    # Input flags
-    ## Parameters for datasets
-    args = parse_args()
-    data_path = args.features_path
-    label_path = args.targets_path
-    snapshot_nr = args.snapshot_nr
-    voxle_nr = args.voxle_nr
-    ## Parameters for ML-algorithm
-    lr = args.lr
-    model_idx = args.model_idx
-    epochs = args.epochs
-    batch_size = args.batch_size
-    loss_weight = args.loss_weight
-    weight_decay = args.weight_decay
-    print_freq = args.print_freq
-    label_type = args.label_type
-    target_class = args.target_class
-    load_model = args.load_model
-    save_name = args.save_name
-    record_results = args.record_results
-    vel = args.vel  # not considered for now
-    normalize = args.normalize
-
-    # Define which cubes go into which dataset (train, test, validate)
-    random_idx = 1
-    pos = list(np.arange(0, voxle_nr, 32))  # sub-divide each voxel by 32^3
-    ranges = list(product(pos, repeat=3))  # all combinations of 3 elements
-    random.seed(7)
-    if random_idx == 1:
-        random.shuffle(ranges)
-        train_data = ranges[: int(np.round(len(ranges) * 0.6))]
-        val_data = ranges[
-            int(np.round(len(ranges) * 0.6)) : int(np.round(len(ranges) * 0.8))
-        ]
-        test_data = ranges[int(np.round(len(ranges) * 0.8)) :]
-    else:
-        train_data, val_data, test_data = [], [], []
-        for i in range(0, data_range, 32):
-            for j in range(0, data_range, 32):
-                for k in range(0, data_range, 32):
-                    idx = (i, j, k)
-                    if i <= 416 and j <= 416:
-                        val_data.append(idx)
-                    elif i >= 484 and j >= 448 and k >= 448:
-                        test_data.append(idx)
-                    else:
-                        train_data.append(idx)
-
-    # print('train_data', train_data[:10], len(train_data))
-    # print('test_data', test_data[:10], len(test_data))
-
-    # Initialize datasets
-    training_set = Dataset(
-        train_data,
-        "%sdark_matter_only_s%d_v%d_dm.npy" % (data_path, snapshot_nr, voxle_nr),
-        "%sfull_physics_s%d_v%d_dm.npy" % (label_path, snapshot_nr, voxle_nr),
-        cat=label_type,
-        reg=target_class,
-    )
-    validation_set = Dataset(
-        val_data,
-        "%sdark_matter_only_s%d_v%d_dm.npy" % (data_path, snapshot_nr, voxle_nr),
-        "%sfull_physics_s%d_v%d_dm.npy" % (label_path, snapshot_nr, voxle_nr),
-        cat=label_type,
-        reg=target_class,
-        normalize=normalize,
-    )
-    testing_set = Dataset(
-        test_data,
-        "%sdark_matter_only_s%d_v%d_dm.npy" % (data_path, snapshot_nr, voxle_nr),
-        "%sfull_physics_s%d_v%d_dm.npy" % (label_path, snapshot_nr, voxle_nr),
-        cat=label_type,
-        reg=target_class,
-        normalize=normalize,
-    )
-    # Load dataset
-    params = {"batch_size": batch_size, "shuffle": True, "num_workers": 20}
-    training_generator = data.DataLoader(training_set, **params)
-    validation_generator = data.DataLoader(validation_set, **params)
-    testing_generator = data.DataLoader(testing_set, **params)
-
-    # Build model
-    dim_out = 1
-    if vel == 1:
-        dim_in = 4
-    else:
-        dim_in = 1
-        dim = 1  # need to be changed later
-
-    model = SimpleUnet(dim, target_class).to(device)
-
-    if target_class == 0:
-        # Classifier
-        criterion = nn.CrossEntropyLoss(
-            weight=get_loss_weight(loss_weight, num_class=2)
-        ).to(device)
-    else:
-        # Regresssion
-        criterion = weighted_nn_loss(loss_weight)
-
-    optimizer = torch.optim.Adam(model.parameters(), lr, weight_decay=weight_decay)
-    initial_loss(
-        training_generator, validation_generator, model, criterion, target_class
-    )
-
-    # for epoch in range(epochs):
-    #    adjust_learning_rate(lr, optimizer, epoch)
-    #    train(
-    #        training_generator,
-    #        model,
-    #        criterion,
-    #        optimizer,
-    #        epoch,
-    #        print_freq,
-    #        target_class=target_class,
-    #    )
-    #    # evaluate on validation set
-    #    validate(
-    #        validation_generator,
-    #        model,
-    #        criterion,
-    #        epoch,
-    #        target_class=target_class,
-    #        save_name=save_name,
-    #    )
-
-    # if len(plot_label) == 0:
-    #    plot_label = "_" + str(target_class) + "_" + str(model_idx) + "_"
-
-    # train_plot(
-    #    TRAIN_LOSS,
-    #    VAL_LOSS,
-    #    VAL_ACC,
-    #    VAL_RECALL,
-    #    VAL_PRECISION,
-    #    target_class,
-    #    plot_label=plot_label,
-    # )
-
-    # if target_class == 0:
-    #    if record_results:
-    #        args = parse_args()
-    #        f = open("all_results", "a+")
-    #        f.write("arguments: %s" % (args) + "\n")
-    #        f.write(
-    #            "Test Loss {BEST_VAL_LOSS:.5f},\
-    #             Test Accuracy {BEST_ACC:.4f},\
-    #             Test Recall {BEST_RECALL:.4f},\
-    #             Precision {BEST_PRECISION:.4f},\
-    #             F1 score  {BEST_F1SCORE:.4f}\n".format(
-    #                BEST_VAL_LOSS=BEST_VAL_LOSS,
-    #                BEST_ACC=BEST_ACC,
-    #                BEST_RECALL=BEST_RECALL,
-    #                BEST_PRECISION=BEST_PRECISION,
-    #                BEST_F1SCORE=BEST_F1SCORE,
-    #            )
-    #        )
-    #        f.close()
+# *************************************************** INPUT PARAMETERS ******************************************************#
+partition = {'train': ['id-1', 'id-2', 'id-3'], 'validation': ['id-4']}
+BATCH_SIZE = 2
+LEARNING_RATE = 0.001
+MAX_EPOCHS = 2
 
 
-if __name__ == "__main__":
-    main()
+# ************************************************* INITIALIZE NETWORK ******************************************************#
+
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
+
+
+training_set = Dataset(partition["train"])
+training_generator = data.DataLoader(
+    training_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=20
+)
+
+validation_set = Dataset(partition["validation"])
+validation_generator = data.DataLoader(
+    validation_set, batch_size=BATCH_SIZE, shuffle=False
+)
+
+# initialize model
+model = SegNet().to(device)
+model = model.double()
+#summary(model, input_size=(1, 32, 32, 32))
+
+# set loss function and optimizer
+# TODO: weigths
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+
+# *************************************************** TRAINING LOOP ******************************************************#
+
+TRAIN_LOSS, VAL_LOSS = [], []
+
+for epoch in range(MAX_EPOCHS):
+    for local_batch, local_labels in training_generator:
+        # Transfer to GPU
+        local_batch, local_labels = local_batch.to(device), local_labels.to(device)
+        local_batch = local_batch.unsqueeze(1).double()
+        local_labels = local_labels.unsqueeze(1).double()
+
+        # Forward pass
+        outputs = model(local_batch)
+        loss = criterion(outputs, local_labels)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        TRAIN_LOSS.append(loss)
+        # TODO: save loss as function of number of dark matter particles per voxel
+
+    if (epoch + 1) % 1 == 0:
+        print("Epoch [{}/{}],  Loss: {:.4f}".format(epoch + 1, MAX_EPOCHS, loss.item()))
+
+    with torch.set_grad_enabled(False):
+        for local_batch, local_labels in validation_generator:
+            # Transfer to GPU
+            local_batch, local_labels = local_batch.to(device), local_labels.to(device)
+            local_batch = local_batch.unsqueeze(1).double()
+            local_labels = local_labels.unsqueeze(1).double()
+
+            outputs = model(local_batch)
+            val_loss = criterion(outputs, local_labels)
+            VAL_LOSS.append(val_loss)
+
+
+# Save training summary
+# Save train/val losses
